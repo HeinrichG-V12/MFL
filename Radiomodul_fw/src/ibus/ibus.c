870411de386cd926d9e3a0bf0ccf36536c0bc10e
Heinrich <60735254+HeinrichG-V12@users.noreply.github.com>
2019-11-02 14:45:59 +0000

    added ad5243 added ibus_network some corrections and refactorings

--- Radiomodul_fw/src/ibus/ibus.c
+++ Radiomodul_fw/src/ibus/ibus.c
@@ -8,6 +8,9 @@
 #include "ibus.h"
 
 void send_message( ibus_msg *msg ) {
+
+	update_crc(msg);
+
 	NVIC_DisableIRQ(UART_IRQn);
 	send_char(msg->src);
 	send_char(msg->len);
@@ -26,13 +29,11 @@ void test( void ) {
 	msg.src = 0xD0;
 	msg.len = 5;
 	msg.dst = 0xBF;
+	msg.func = 0x5C;
 
-	msg.data = (uint8_t*) calloc(3, sizeof(uint8_t));
-
-	msg.data[0] = 0x5C;
-	msg.data[1] = 0xEC;
-	msg.data[2] = 0x00;
-
+	msg.data = (uint8_t*) calloc(2, sizeof(uint8_t));
+	msg.data[0] = 0xEC;
+	msg.data[1] = 0x00;
 	update_crc (&msg);
 }
 
@@ -41,22 +42,11 @@ void update_crc (ibus_msg *msg) {
 	crc ^= msg->src;
 	crc ^= msg->len;
 	crc ^= msg->dst;
-	for (int i = 0; i <= msg->len-2; i++) {
+	crc ^= msg->func;
+	for (int i = 0; i <= msg->len - 2; i++) {
 		crc ^= msg->data[i];
 	}
 	msg->crc = crc;
 }
 
-void set_bright (uint8_t value) {
-	ibus_msg msg;
-	msg.src = 0xD0;
-	msg.len = 5;
-	msg.dst = 0xBF;
-	msg.data = (uint8_t*) calloc(3, sizeof(uint8_t));
-	msg.data[0] = 0x5C;
-	msg.data[1] = value;
-	msg.data[2] = 0x00;
 
-	update_crc (&msg);
-	send_message(&msg);
-}
