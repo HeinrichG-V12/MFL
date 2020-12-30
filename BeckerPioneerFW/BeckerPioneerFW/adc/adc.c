77370102ccc820013ebc0eee761d7ce6bd71fc62
HeinrichG-V12 <heinrich@bmw-rudel.de>
2020-12-30 21:16:58 +0100

    Migrated to new Atmel Studio version

--- BeckerPioneerFW/BeckerPioneerFW/adc/adc.c
+++ BeckerPioneerFW/BeckerPioneerFW/adc/adc.c
@@ -11,20 +11,16 @@ uint8_t adc_value = 0;
 
 void adc_init (void)
 {
-	ADMUX = (1 << REFS0)|(1 << ADLAR);	// adc on, left justified, avcc reference, adc0 input
-	ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADIE)|(1<<ADATE)|(1<<ADPS2)|(1<<ADPS0); // ad enabled, interrupt enabled, prescaler = 128
+	//ADMUX = (1 << REFS0)|(1 << ADLAR);	// left justified, avcc reference, adc0 input
+	ADMUX = (1 << REFS0);	// avcc reference, adc0 input
+	ADCSRA = (1 << ADEN)|(1 << ADIE)|(1 << ADATE)|(1 << ADPS2)|(1 << ADPS1)|(1 << ADPS0); // ad enabled, start convertion, interrupt enabled, auto trigger, prescaler = 128 (125kHz)
 	// adcsrb is not used because of free runnging mode
+	
+	ADCSRA |= (1<<ADSC);	// start first convertion
 }
 
 ISR(ADC_vect)
 {
-	// If the result is left adjusted and no more than 8-bit precision is required, it is sufficient to read ADCH
-	if (ADCH > 254) 
-	{
-		adc_value = 254;
-	}
-	else 
-	{
-		adc_value = ADCH;
-	}
+	uint16_t adc_raw_value = ADC;
+	adc_value = adc_raw_value >> 2;	
 }
